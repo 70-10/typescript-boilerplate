17147de5989e0267d2d28be3ac763c59c0f10b9f
70_10 <7010.dev@gmail.com>
2017-11-06 23:46:19 +0900

    [del] project

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export function main() {
-  console.log("Hello, world");
-}
-
-export function twice(num: number) {
-  return num * 2;
-}
--- test/sample.test.ts
+++
@@ -1,9 +0,0 @@
-import * as assert from "power-assert";
-import * as main from "../src";
-
-describe("Sample Test", () => {
-  it("twice", () => {
-    assert(main.twice(1) === 2);
-  });
-});
-
